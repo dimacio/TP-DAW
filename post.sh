7aecd9f083e527354e00db92870cba4b987ee351
dimacio <dimaciodimacio@gmail.com>
2025-06-17 21:44:06 -0300

    borro bash

--- post.sh
+++
@@ -1,7 +0,0 @@
-curl -X POST -H "Content-Type: application/json" -d '{
-      "nombre_id": "sorete en pala",
-      "ubicacion": "baño",
-      "estado": 0,
-      "nivel": 0
-    }' http://localhost:8000/devices
-
